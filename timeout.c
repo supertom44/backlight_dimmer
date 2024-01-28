80a8c3b0b52d037350cad1b49a678ea636b68c48
Tom Horan <t.v.horan@gmail.com>
2024-01-28 07:43:35 +0000

    Update timeout.c
    Updating path

--- timeout.c
+++ timeout.c
@@ -99,9 +99,9 @@ int main(int argc, char* argv[]){
         sleepTime.tv_sec = 0;
         sleepTime.tv_nsec = 100000000L;  /* 1 seconds - larger values may reduce load even more */
 
-	char actual[53] = "/sys/class/backlight/rpi_backlight/actual_brightness";
-	char max[50] = "/sys/class/backlight/rpi_backlight/max_brightness";
-  	char bright[46] = "/sys/class/backlight/rpi_backlight/brightness";
+	char actual[53] = "/sys/class/backlight/10-0054/actual_brightness";
+	char max[50] = "/sys/class/backlight/10-0054/max_brightness";
+  	char bright[46] = "/sys/class/backlight/10-0054/brightness";
 
 	brightfd = fopen(bright, "w");
         if(brightfd == NULL){
